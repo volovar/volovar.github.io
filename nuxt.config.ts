36c5327c7aa1298d20df3324a89f011f6b148407
volovar <michaelvolovar@gmail.com>
2026-01-26 14:09:09 -0500

    chore/move app related files into app folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 });
